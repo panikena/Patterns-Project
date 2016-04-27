73fe4adbb851292e6816a8f590687e78559056e5
panikena <adenisenko@ymail.com>
2016-04-27 19:32:59 +0300

    New LoginFrame properties

--- src/com/panikena/GUI/LoginFrame.java
+++ src/com/panikena/GUI/LoginFrame.java
@@ -3,23 +3,21 @@ package com.panikena.GUI;
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * Created by Artem on 27.04.2016.
- */
 public class LoginFrame extends JFrame {
 
     String name, password;
     JTextField nameField;
     JPasswordField passwordField;
     JLabel nameLabel, passwordLabel;
-    JButton signInButton, cancelButton;
+    JButton signInButton;
     JPanel panel;
 
 
-    LoginFrame(){
+    public LoginFrame(){
+        super("Login");
         this.setLayout(new BorderLayout());
-        this.setSize(new Dimension(600, 400));
-        this.setPreferredSize(new Dimension(600, 400));
+        this.setSize(new Dimension(300, 250));
+        this.setPreferredSize(new Dimension(300, 250));
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int centerX = (screenSize.width - this.getWidth()) / 2;
@@ -27,6 +25,7 @@ public class LoginFrame extends JFrame {
 
         this.setLocation(centerX, centerY);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setResizable(false);
 
         panel = new JPanel();
         this.add(panel);
@@ -35,6 +34,7 @@ public class LoginFrame extends JFrame {
         nameLabel = new JLabel("Name: ");
         passwordField = new JPasswordField();
         passwordLabel = new JLabel("Password: ");
+        signInButton = new JButton("Sign in");
 
         panel.setLayout(new GridBagLayout());
 
@@ -43,19 +43,37 @@ public class LoginFrame extends JFrame {
 
         constraints.gridx = 0;
         constraints.gridy = 0;
-        constraints.insets = new Insets(10, 10, 10, 10);
+        constraints.insets = new Insets(25, 0, 10, 0);
+        constraints.weightx = 1;
+        constraints.anchor = GridBagConstraints.EAST;
         panel.add(nameLabel, constraints);
-
-        constraints.gridy = 1;
-        panel.add(nameField, constraints);
-
 
         constraints.gridx = 1;
         constraints.gridy = 0;
+        constraints.anchor = GridBagConstraints.CENTER;
+        nameField.setPreferredSize(new Dimension(100, 30));
+        panel.add(nameField, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.insets = new Insets(10, 0, 10, 0);
+        constraints.anchor = GridBagConstraints.EAST;
         panel.add(passwordLabel, constraints);
 
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        passwordField.setPreferredSize(new Dimension(100, 30));
+        panel.add(passwordField, constraints);
 
 
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        panel.add(signInButton, constraints);
+
+        this.setVisible(true);
 
     }
 }
