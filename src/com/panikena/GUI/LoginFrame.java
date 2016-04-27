8c7baebd4d9166164965992aaa0969a96e3fa2d6
panikena <adenisenko@ymail.com>
2016-04-27 19:08:31 +0300

    Committing LoginFrame progress

--- src/com/panikena/GUI/LoginFrame.java
+++ src/com/panikena/GUI/LoginFrame.java
@@ -11,6 +11,7 @@ public class LoginFrame extends JFrame {
     String name, password;
     JTextField nameField;
     JPasswordField passwordField;
+    JLabel nameLabel, passwordLabel;
     JButton signInButton, cancelButton;
     JPanel panel;
 
@@ -27,9 +28,33 @@ public class LoginFrame extends JFrame {
         this.setLocation(centerX, centerY);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        panel = new JPanel();
+        this.add(panel);
 
         nameField = new JTextField();
+        nameLabel = new JLabel("Name: ");
         passwordField = new JPasswordField();
+        passwordLabel = new JLabel("Password: ");
+
+        panel.setLayout(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        panel.add(nameLabel, constraints);
+
+        constraints.gridy = 1;
+        panel.add(nameField, constraints);
+
+
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        panel.add(passwordLabel, constraints);
+
+
 
 
     }
